9d54ee93e504b286993f9d3758bb184381cf190b
soaringswine <dev.null.02@gmail.com>
2025-07-05 18:12:47 -0400

    Fix typos and add test

--- run.bat
+++ run.bat
@@ -1,1 +1,2 @@
 docker build --rm -f "Dockerfile" -t lazyrecon_docker:latest . && docker run -v %cd%\lazyrecon_results:/home/lazyrecon_user/tools/lazyrecon/lazyrecon_results/ lazyrecon_docker -d %1
+
--- run.sh
+++ run.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+
+if [ $# -lt 1 ]; then
+    echo "Usage: $0 <domain>" >&2
+    exit 1
+fi
+
 docker build --rm -f "Dockerfile" -t lazyrecon_docker:latest . \
-&& docker run --user $(id -u):$(id -g) -v $(pwd)/lazyrecon_results:/home/lazyrecon_user/tools/lazyrecon/lazyrecon_results/ lazyrecon_docker -d $1
+&& docker run --user $(id -u):$(id -g) -v $(pwd)/lazyrecon_results:/home/lazyrecon_user/tools/lazyrecon/lazyrecon_results/ lazyrecon_docker -d "$1"
+
